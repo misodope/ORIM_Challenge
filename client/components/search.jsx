511ef5f250f1afeda30edb4a8615fadaa34161f1
Jerry Chen <jieningjchen@gmail.com>
2018-05-12 12:48:59 -0400

    Adding enter button keydown functionality for searching

--- client/components/search.jsx
+++ client/components/search.jsx
@@ -9,6 +9,7 @@ class Search extends React.Component {
     }
 
     this.handleChange = this.handleChange.bind(this);
+    this.handleKeyDown = this.handleKeyDown.bind(this);
   }
 
   handleChange(e) {
@@ -17,10 +18,16 @@ class Search extends React.Component {
     })
   }
 
+  handleKeyDown(e) {
+    if (e.keyCode === 13) {
+      this.props.searchBooks(this.state.searchTitle);
+    }
+  }
+
   render() {
     return (
       <div className='search'>
-        <input type='text' placeholder='Search Titles...' onChange={(e) => this.handleChange(e)} />
+        <input type='text' placeholder='Search Titles...' onChange={this.handleChange} onKeyDown={this.handleKeyDown}/>
         <button type='button' onClick={() => this.props.searchBooks(this.state.searchTitle)}><i className="fas fa-search" /></button>
       </div>
     )
--- server/index.js
+++ server/index.js
@@ -9,34 +9,22 @@ const port = 3000;
 app.use(parser.json());
 app.use(express.static(path.join(__dirname, '../client/public')));
 
+/* ************************** GET METHODS ************************* */
+//Setting globally accessible searchedBooks array, and maxBooks lengths.
 let maxBooks = books.length;
 let searchedBooks = []
 
-/* ************************** GET METHODS ************************* */
-
 app.get('/next/books', (req, res) => {
-  let showBooks, currentBooks, lastBooks, currentBookDisplay;
+  let showBooks;
+  let currentBookDisplay = parseInt(req.query.currentBookDisplay);
+  let lastBooks = currentBookDisplay;
+  let currentBooks = currentBookDisplay + 10;
+  //If there are no books that have been searched for, look at whole collection of books
   if (!searchedBooks.length) {
-    //Converting strings to valid numbers
-    currentBookDisplay = parseInt(req.query.currentBookDisplay);
-    lastBooks = currentBookDisplay;
-
-    //Set the current index of books
-    currentBooks = currentBookDisplay + 10;
-
-    //Get the next 10 books to display
     showBooks = books.slice(currentBookDisplay, currentBooks);
   } else {
-    //look through searched books
-    //Converting strings to valid numbers
-    currentBookDisplay = parseInt(req.query.currentBookDisplay);
-    lastBooks = currentBookDisplay;
-
-    //Set the current index of books
-    currentBooks = currentBookDisplay + 10;
-
-    //Get the next 10 books to display
-    showBooks = searchedBooks.slice(currentBookDisplay, currentBookDisplay + 10);
+    //Else books there are searched books, look at the current searched books.
+    showBooks = searchedBooks.slice(currentBookDisplay, currentBooks);
   }
 
   res.status(200).send({ showBooks, lastBooks, currentBooks, maxBooks });
@@ -46,7 +34,7 @@ app.get('/previous/books', (req, res) => {
   let showBooks, currentBooks, lastBooks;
 
   let lastBookDisplay = parseInt(req.query.lastBookDisplay);
-
+  //If there are no searched books, look at the entire book collection
   if ((lastBookDisplay !== null || lastBookDisplay > 0) && !searchedBooks.length) {
     showBooks = books.slice(lastBookDisplay - 10, lastBookDisplay);
 
@@ -72,26 +60,30 @@ app.get('/previous/books', (req, res) => {
 })
 
 app.get('/search/books', (req, res) => {
-  //Reset searched books array to 0
+  //Reset searched books array to 0 everytime a new search happens.
   searchedBooks.length = 0;
-
+  //Set searched query to all lowercase letters
   let searchLowerCase = req.query.search.toLowerCase();
+  //Intialize books index variables
   let showBooks, currentBooks, lastBooks, currentBookDisplay;
-
+  //Loops through each book and check if the book title includes the search query.
   for (let i = 0; i < books.length; i++) {
     let book = books[i];
     if (book.title.toLowerCase().includes(searchLowerCase)) {
       searchedBooks.push(book);
     }
   }
+  //If there are searchedBooks set the indexes and look at the searchedBooks array.
   if (searchedBooks.length > 0) {
     currentBookDisplay = parseInt(req.query.currentBookDisplay);
     currentBooks = currentBookDisplay + 10;
     lastBooks = currentBookDisplay;
-    showBooks = searchedBooks.slice(currentBookDisplay, currentBooks);
     maxBooks = searchedBooks.length;
+    showBooks = searchedBooks.slice(currentBookDisplay, currentBooks);
 
     res.status(200).send({ showBooks, lastBooks, currentBooks, maxBooks });
+  } else {
+    res.status(404).send();
   }
 })
 
